8b486becc3c87919b4d1a13395ee320a486b582c
Cameron Lassiter <classit5@uncc.edu>
2023-09-26 14:14:39 -0400

    updated backend

--- backend/app.py
+++
@@ -1,8 +0,0 @@
-#meant to implement firbase
-#default_app = firebase_admin.initialize_app()
-
-# imports
-import os                 # os is used to get environment variables IP & PORT
-from flask import Flask   # Flask is the web app that we will customize
-
-app = Flask(__name__)     # create an app
